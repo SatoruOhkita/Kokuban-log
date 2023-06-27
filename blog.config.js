56baadfcc64962c7353e902518220f46393e73ba
大北悟 <satoru.ohkita@gmail.com>
2023-06-27 18:16:18 +0900

    ディスクリプション変更、リモートリポジトリのURL変更

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'SatoruOhkita',
   email: '',
   link: 'https://nobelium.vercel.app',
-  description: '備忘録です。板書や学びのメモ。',
+  description: '備忘録。板書や学びのメモ。',
   lang: 'ja-JP', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Tokyo', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
