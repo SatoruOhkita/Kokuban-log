5364a635933efb8c5569002329dc6a459b111fe4
大北悟 <satoru.ohkita@gmail.com>
2023-06-27 12:12:31 +0900

    ナビゲーションの編集、背景色の変更、TwitterURL編集

--- components/Header.js
+++ components/Header.js
@@ -9,10 +9,14 @@ const NavBar = () => {
   const BLOG = useConfig()
   const locale = useLocale()
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
-    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
+    // { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
+    { id: 0, name: 'Blog', to: BLOG.path || '/', show: true },
+    // { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
+    { id: 1, name: 'About', to: '/about', show: BLOG.showAbout },
+    // { id: 2, name: 'AboutMe', to: '/experiment', show: true},
+    { id: 2, name: locale.NAV.RSS, to: '/feed', show: false, external: false },
+    // { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true }
+    { id: 3, name: 'Search', to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
